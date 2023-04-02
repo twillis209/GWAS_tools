9f0453077742d1fe9482f178e5d3bf08e95f2595
Tom Willis <tw395@cam.ac.uk>
2023-04-02 04:58:18 +0100

    Simplified directory structure

--- special_cases/Buko/Buko_dnd1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-wget https://humandbs.biosciencedbc.jp/files/hum0014/hum0014.v17.BA.v1.zip;
-wget https://humandbs.biosciencedbc.jp/files/hum0014/hum0014.v17.CHB.v1.zip;
-wget https://humandbs.biosciencedbc.jp/files/hum0014/hum0014.v17.CHC.v1.zip;
-wget https://humandbs.biosciencedbc.jp/files/hum0014/hum0014.v17.COPD.v1.zip;
-wget https://humandbs.biosciencedbc.jp/files/hum0014/hum0014.v17.EM.v1.zip
