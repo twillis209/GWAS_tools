1e28855c23fcaccb1b98d4800d9897e43fb82852
Tom Willis <tw395@cam.ac.uk>
2023-04-14 07:15:24 +0100

    Committing ahead of copying over changes from pid_cfdr_pipeline (not yet using modules)

--- workflow/scripts/merge_liftovered_rows_with_sumstats.R
+++ workflow/scripts/merge_liftovered_rows_with_sumstats.R
@@ -3,6 +3,9 @@ setDTthreads(snakemake@threads)
 
 dat <- fread(snakemake@input[['sumstats']], header = T, sep = '\t')
 bedfile <- fread(snakemake@input[['lifted']], header = F, sep = '\t')
+build <- fread(snakemake@input[['build']], header = T, sep = '\t')
+
+assembly <- names(which.max(build[1]))
 
 names(bedfile) <- c('CHR38', 'BP38', 'BP2', 'SNPID')
 
@@ -11,5 +14,9 @@ bedfile <- bedfile[, .(SNPID, CHR38, BP38)]
 dat <- merge(dat, bedfile, by = 'SNPID')
 
 dat[, CHR38 := stringr::str_remove(CHR38, 'chr')]
+
+if(assembly == 'hg38') {
+  dat[, c('CHR', 'BP') := NULL]
+}
 
 fwrite(dat, file = snakemake@output[[1]], sep = '\t')
